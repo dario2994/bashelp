8f64d7b07ef45f10a8d3f46f7bd624e6432d1bd9
Federico Glaudo <dario2994@gmail.com>
2014-09-26 16:02:07 +0200

    Improved install.sh and uninstall.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ronn -r docs/manpage.md
-sudo cp bashelp.py /usr/local/bin/bashelp
-sudo mkdir -p /usr/local/man/man1
-sudo cp docs/bashelp.1 /usr/local/man/man1/bashelp.1.gz
+cp bashelp.py /usr/local/bin/bashelp
+
+pandoc -s -w man docs/manpage.md -o docs/bashelp.1
+mkdir -p /usr/local/man/man1
+mv docs/bashelp.1 /usr/local/man/man1/bashelp.1.gz
 
 echo 'bashelp has been succesfully installed'
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f /usr/local/man/man1/bashelp.1.gz
+rm -f /usr/local/bin/bashelp
+rm -rf ~/.bashelp
+
+echo 'bashelp has been completely uninstalled'
